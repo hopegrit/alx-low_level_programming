d1f014bc84360801248ad1e4ba62b520d70acdfd
hopegrit <emmanuel_oluwunmi@yahoo.com>
2022-09-22 09:01:47 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main-printing in C
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingua puzzle").
+		return (0);
+}
