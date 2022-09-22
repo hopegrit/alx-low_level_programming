cff6c01f4411d18120f7ee2705c4de29ff029729
hopegrit <emmanuel_oluwunmi@yahoo.com>
2022-09-22 11:43:02 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints a text
+ *
+ * Return: Alaways 0 (successful)
+ */
+int main (void)
+{
+	Print("milk programmer, but theoutcome is a work of art,ln");
+	return (0);
+}
